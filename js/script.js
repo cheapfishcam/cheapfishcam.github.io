9cbfcca67e62f78a8d4af4f6155e4c20ef853d3d
Mohamed T Safieldeen <40473487+cheapfishcam@users.noreply.github.com>
2018-12-15 19:44:22 +0200

    ubcb

--- js/script.js
+++ js/script.js
@@ -47,6 +47,17 @@ var remote;          // ID of the remote peer -- set once they send an offer
 
 
 //Makes screen fullscreen
+//expirment
+document.getElementById('aywallo').value;
+document.getElementById('taneekda').value;
+document.getElementById('henaho').addEventListener('click',function(){
+  localStorage.setItem(taneekda, aywallo);
+
+});
+console.log(localStorage);
+
+//expirment
+
 
  var fulscrn  = document.getElementById('fullscrnbtn');
  var html = document.documentElement;

fb4d2d13bd7c38ee21f852fda7bba5510f1ccab2
BassemSafieldeen <bassemsafieldeen@gmail.com>
2019-10-02 19:07:08 +0200

    Trying a day and night tool called terminator from Leaflet
    Only need the getCountryName() function for the radio now.
    
    Also, there is a sunset function in leaflet that I might use instead of terminator in the future.

--- js/script_MapMode.js
+++ js/script_MapMode.js
@@ -126,6 +126,16 @@ function getCountryName(latlng){
         });
 }
 
+//night and day zones
+var t = L.terminator();
+t.addTo(map);
+setInterval(function(){updateTerminator(t)}, 500);
+function updateTerminator(t) {
+  var t2 = L.terminator();
+  t.setLatLngs(t2.getLatLngs());
+  t.redraw();
+}
+
 
 
 

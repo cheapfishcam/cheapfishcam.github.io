5b0ed7ab8a4007108d4968313b62293ecb2cfaa3
Mohamed T Safieldeen <40473487+cheapfishcam@users.noreply.github.com>
2019-10-08 17:07:52 +0200

    mm

--- js/world_Radios.js
+++ js/world_Radios.js
@@ -1,3 +1,4 @@
 var radios = [{country: "Netherlands", src: "https://tunein.com/embed/player/s77881?autoplay=true", latlng: L.latLng(52.3667,4.8945)}, 
-              {country: "Germany", src: "https://tunein.com/embed/player/s55698?autoplay=true", latlng: L.latLng(52.5200,13.4050)}
+              {country: "Germany", src: "https://tunein.com/embed/player/s55698?autoplay=true", latlng: L.latLng(52.5200,13.4050)},
+              {country:"Egypt" , src: "https://tunein.com/embed/player/s65628?autoplay=true", lating: L.latLng(30.0444,31.2357)}
              ];

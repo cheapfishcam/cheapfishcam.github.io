ed2c0cec8fe2d7ef6d5ed41c0a051ae0744e5495
BassemSafieldeen <bassemsafieldeen@gmail.com>
2020-01-13 19:25:04 +0100

    added riyadh radio

--- js/world_Radios.js
+++ js/world_Radios.js
@@ -1,4 +1,5 @@
 var radios = [{country: "Netherlands", src: "https://tunein.com/embed/player/s77881?autoplay=true", latlng: L.latLng(52.3667,4.8945)}, 
               {country: "Germany", src: "https://tunein.com/embed/player/s55698?autoplay=true", latlng: L.latLng(52.5200,13.4050)},
-              {country:"Egypt" , src:  "https://tunein.com/embed/player/s210036?autoplay=true" , latlng: L.latLng(30.0444,31.2357)}
+              {country:"Egypt" , src:  "https://tunein.com/embed/player/s210036?autoplay=true" , latlng: L.latLng(30.0444,31.2357)},
+              {country:"SaudiArabia" , src:  "https://tunein.com/embed/player/s135623?autoplay=true" , latlng: L.latLng(24.7136,46.6753)}
              ];
